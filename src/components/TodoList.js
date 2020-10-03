5ae266b2092d745f0c7243ae9252357677345128
Theodore-A <adaramolatheodore@gmail.com>
2020-10-03 08:56:58 +0100

    learning react.js

---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Todo from "./Todo";
+
+function TodoList({ todos, setTodos, filteredTodos }) {
+  return (
+    <div>
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo
+            todo={todo}
+            todos={todos}
+            setTodos={setTodos}
+            key={todo.id}
+            text={todo.text}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
